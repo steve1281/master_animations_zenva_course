395f38bf642249a15eee29abc36609465966ce08
Steve Falcigno <steve1281@hotmail.com>
2025-05-10 07:36:21 -0400

    collide with coin

--- Scripts/MiniProject/Coin.cs
+++ Scripts/MiniProject/Coin.cs
@@ -6,13 +6,23 @@ public partial class Coin : Area2D
 {
     [Export] public Texture2D[] coins;
     [Export] public Sprite2D coin;
+    [Export] public AnimationPlayer animPlayer;
 
     private int spriteIndex;
 
     public override void _Ready()
     {
+        BodyEntered += HandleBodyEntered;
+        
         spriteIndex = (int)(GD.Randi() % coins.Length);
         coin.Texture = coins[spriteIndex];
     }
 
+    private void HandleBodyEntered(Node2D body)
+    {
+        //GD.Print($"Coin::HandleBodyEntered:body.Name = {body.Name}");
+        if (body.Name == "Player") {
+            animPlayer.Play("CollidedWithPlayer");
+        }
+    }
 }
